bec0574e6cc666b60a7492d2e15f23c574e1498d
395currahee <395currahee@penguin>
2020-01-16 12:56:55 -0500

    add chapter code

--- rust-code/rectangles/src/main.rs
+++ rust-code/rectangles/src/main.rs
@@ -5,15 +5,38 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {        // this is a method when inside an `impl`
+        self.width * self.height
+    }
+    
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     
     let rect1 = Rectangle { width: 30, height: 50 };
-    let rect2 = Rectangle { width: 24, height: 42 };
-    println!("rect1 isa' {:#?}", rect2);
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };  
+
+    println!("can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("can rect1 hold rect3? {}", rect1.can_hold(&rect3));     
 }
 
+
+/*
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
 } // no semi-colon marks an expression. 
 
+output: 
+    Finished dev [unoptimized + debuginfo] target(s) in 2.15s
+     Running `target/debug/rectangles`
+can rect1 hold rect2? true
+can rect1 hold rect3? false
+
+
+*/
 
---
+++ rust-code/rectangles/target/debug/deps/rectangles-0db069fb0d795d03.d
@@ -0,0 +1,5 @@
+/home/395currahee/projects/der0pa.github.io/rust-code/rectangles/target/debug/deps/rectangles-0db069fb0d795d03: src/main.rs
+
+/home/395currahee/projects/der0pa.github.io/rust-code/rectangles/target/debug/deps/rectangles-0db069fb0d795d03.d: src/main.rs
+
+src/main.rs:
